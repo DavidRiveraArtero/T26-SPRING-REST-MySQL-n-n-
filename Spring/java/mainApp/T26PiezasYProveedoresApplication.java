10e2daac969493477be0e7e28f8dd92fa0c16d7b
DavidRiveraArtero <davidtoy222@gmail.com>
2021-08-26 19:32:21 +0200

    Version 1.0

---
+++ Spring/java/mainApp/T26PiezasYProveedoresApplication.java
@@ -0,0 +1,13 @@
+package mainApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T26PiezasYProveedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T26PiezasYProveedoresApplication.class, args);
+	}
+
+}
